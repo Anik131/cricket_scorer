2e0481b6423cbe55c8b60f7654dab529b6ee3107
Anik131 <abdulhaianik13@gmail.com>
2022-04-08 00:09:18 +0600

    Updated

--- batvsbowl.js
+++ batvsbowl.js
@@ -1,90 +1,130 @@
-// Header 
-let hostName= localStorage.getItem("HostName");
-let VisitorName =localStorage.getItem("VisitorName");
-let hostNameVsVisitorName = hostName.concat(" vs ",VisitorName);
-let title= document.getElementById("headerTeamvsTeam");
+// Header
+let hostName = localStorage.getItem("HostName");
+let VisitorName = localStorage.getItem("VisitorName");
+let hostNameVsVisitorName = hostName.concat(" vs ", VisitorName);
+let title = document.getElementById("headerTeamvsTeam");
 title.innerText = hostNameVsVisitorName;
 title.style.color = "Green";
 
 let batsmanOne = JSON.parse(localStorage.getItem("batsmanOne"));
 let stricker = document.getElementById("stricker1");
-stricker.innerText= batsmanOne.name;
+stricker.innerText = batsmanOne.name;
 
 let batsmanTw0 = JSON.parse(localStorage.getItem("batsmanTwo"));
 let stricker2 = document.getElementById("stricker2");
-stricker2.innerText= batsmanTw0.name;
+stricker2.innerText = batsmanTw0.name;
 
 let oBowler = JSON.parse(localStorage.getItem("bowlerOne"));
 let bowlerOne = document.getElementById("tableCurrentBowler");
 tableCurrentBowler.innerText = oBowler.name;
 
 // let stricker1OneRun= JSON.parse(localStorage.getItem("stricker1Run"));
-// let 
+// let
 
 // Strike Rate = (Runs Scored / Balls faced) * 100
 // Economy Rate = Runs Conceded ÷ Overs Bowled
 
-
 // Zero Run
-function zeroRun(){
+function zeroRun() {
+  let batsmanOne = localStorage.getItem("batsmanOne");
+  batsmanOne = JSON.parse(batsmanOne);
 
-if (localStorage.getItem("currentBatsman")=="batsmanOne"){
-    let batsmanOne = localStorage.getItem("batsmanOne");
-    batsmanOne = JSON.parse(batsmanOne);
-    batsmanOne.bowl = batsmanOne.bowl +1;
-}
-else{
-    if (localStorage.getItem("currentBatsman")=="batsmanTw0"){
-        let batsmanTw0 = localStorage.getItem("batsmanTw0");
-        batsmanTw0 = JSON.parse(batsmanTw0);
-        batsmanTw0.bowl = batsmanTw0.bowl +1;
-    }
-}
-let bowlerOne= localStorage.getItem('bowlerOne');
-bowlerOne= JSON.parse(bowlerOne);
-let bowl = parseFloat(bowlerOne.over) + 0.1;
-bowl = parseFloat(bowl).toFixed(1);
-bowlerOne.over = bowl;
-if((bowlerOne.over % 1).toFixed(1)== 0.6){
+  let batsmanTw0 = localStorage.getItem("batsmanTwo");
+  batsmanTw0 = JSON.parse(batsmanTw0);
+
+  if (localStorage.getItem("currentBatsman") == batsmanOne.name) {
+    batsmanOne.bowl = batsmanOne.bowl + 1;
+    localStorage.setItem("batsmanOne", JSON.stringify(batsmanOne));
+  } else if (localStorage.getItem("currentBatsman") == batsmanTw0.name) {
+    batsmanTw0.bowl = batsmanTw0.bowl + 1;
+    localStorage.setItem("batsmanTwo", JSON.stringify(batsmanTw0));
+  }
+
+  let bowlerOne = localStorage.getItem("bowlerOne");
+  bowlerOne = JSON.parse(bowlerOne);
+  let bowl = parseFloat(bowlerOne.over) + 0.1;
+  bowl = parseFloat(bowl).toFixed(1);
+  bowlerOne.over = bowl;
+  if ((bowlerOne.over % 1).toFixed(1) == 0.6) {
     bowlerOne.over = Math.ceil(bowlerOne.over);
+  }
 
+  localStorage.setItem("bowlerOne", JSON.stringify(bowlerOne));
+  updateInformation(batsmanOne, batsmanTw0, bowlerOne);
 }
-}
+
 // One Run
 
-function oneRun(){
-
+function oneRun() {
+  // localStorage.getItem("currentBatsman") == "batsmanOne" ? localStorage.setItem('currentBatsman', 'batsmanTwo') : localStorage.setItem('currentBatsman', 'batsmanOne');
 }
 
 // Two Run
 
-function twoRun(){
-
-
-}
+function twoRun() {}
 
 // Three Run
 
-function threeRun(){
-
-}
+function threeRun() {}
 
 // Four Run
 
-function fourRun(){
-
-}
+function fourRun() {}
 
 // Five Run
 
-function fiveRun(){
-
-}
+function fiveRun() {}
 // Six Run
 
-function sixRun(){
-
+function sixRun() {}
+function dotDot() {
+  console.log("clicked");
 }
-function dotDot(){
-    console.log("clicked");
+
+function updateInformation(batsmanOne, batsmanTw0, bowlerOne) {
+  let batBallTeamCheck = JSON.parse(localStorage.getItem("batBallTeamCheck"));
+
+  teams = JSON.parse(localStorage.getItem("teams"));
+  if (batBallTeamCheck.battingTeam == localStorage.getItem("HostName")) {
+    teams.forEach((team) => {
+      if (team.name == localStorage.getItem("HostName")) {
+        console.log(batsmanOne.name);
+        console.log(batsmanTw0.name);
+        team.batsmanArray.forEach((batsMan, i) => {
+          if (batsMan.name == batsmanOne.name) {
+            team.batsmanArray[i] = batsmanOne;
+          } else if (batsMan.name == batsmanTw0.name) {
+            team.batsmanArray[i] = batsmanTw0;
+          }
+        });
+      } else if (team.name == localStorage.getItem("VisitorTeam")) {
+        team.bowlerArray.forEach((bowler, i) => {
+          if (bowler.name == bowlerOne.name) {
+            team.bowlerArray[i] = bowlerOne;
+          }
+        });
+      }
+    });
+  } else if (
+    batBallTeamCheck.battingTeam == localStorage.getItem("VisitorName")
+  ) {
+    teams.forEach((team) => {
+      if (team.name == localStorage.getItem("VisitorName")) {
+        team.batsmanArray.forEach((batsMan, i) => {
+          if (batsMan.name == batsmanOne.name) {
+            team.batsmanArray[i] = batsmanOne;
+          } else if (batsMan.name == batsmanTw0.name) {
+            team.batsmanArray[i] = batsmanTw0;
+          }
+        });
+      } else if (team.name == localStorage.getItem("HostName")) {
+        team.bowlerArray.forEach((bowler, i) => {
+          if (bowler.name == bowlerOne.name) {
+            team.bowlerArray[i] = bowlerOne;
+          }
+        });
+      }
+    });
+  }
+  localStorage.setItem("teams", JSON.stringify(teams));
 }
--- sop.js
+++ sop.js
@@ -1,144 +1,166 @@
 function selectOpeninglayers() {
-	function player1() {
-		let cStricker = document.getElementById("cStricker").value;
-		if (cStricker == "") {
-			document.getElementById("required3").innerHTML =
-				"batsman's name is required";
-			document.getElementById("required3").style.color = "red";
-		} else {
-			document.getElementById("required3").innerHTML = "";
-		}
-	}
-	function player2() {
-		let nonStriker = document.getElementById("nonStriker").value;
-		if (nonStriker == "") {
-			document.getElementById("required4").innerHTML =
-				"batsman's name is required";
-			document.getElementById("required4").style.color = "red";
-		} else {
-			document.getElementById("required4").innerHTML = "";
-		}
-	}
-	function player3() {
-		let oBowler = document.getElementById("oBowler").value;
-		if (oBowler == "") {
-			document.getElementById("required5").innerHTML =
-				"Bowler's name is required";
-			document.getElementById("required5").style.color = "red";
-		} else {
-			document.getElementById("required5").innerHTML = "";
-		}
-	}
-	player1();
-	player2();
-	player3();
+  function player1() {
+    let cStricker = document.getElementById("cStricker").value;
+    if (cStricker == "") {
+      document.getElementById("required3").innerHTML =
+        "batsman's name is required";
+      document.getElementById("required3").style.color = "red";
+    } else {
+      document.getElementById("required3").innerHTML = "";
+    }
+  }
+  function player2() {
+    let nonStriker = document.getElementById("nonStriker").value;
+    if (nonStriker == "") {
+      document.getElementById("required4").innerHTML =
+        "batsman's name is required";
+      document.getElementById("required4").style.color = "red";
+    } else {
+      document.getElementById("required4").innerHTML = "";
+    }
+  }
+  function player3() {
+    let oBowler = document.getElementById("oBowler").value;
+    if (oBowler == "") {
+      document.getElementById("required5").innerHTML =
+        "Bowler's name is required";
+      document.getElementById("required5").style.color = "red";
+    } else {
+      document.getElementById("required5").innerHTML = "";
+    }
+  }
+  player1();
+  player2();
+  player3();
 
-	let tossOwn = localStorage.getItem("toss");
-	let opted = localStorage.getItem("OptedCheck");
+  let tossOwn = localStorage.getItem("toss");
+  let opted = localStorage.getItem("OptedCheck");
 
-	let batsmanArray = [];
-	let bowlerArray = [];
-	let cStricker = document.getElementById("cStricker");
+  let batsmanArray = [];
+  let bowlerArray = [];
+  let cStricker = document.getElementById("cStricker");
 
-	let batsmanOne = {
-		name: cStricker.value,
-		run: 0,
-		bowl: 0,
-		four: 0,
-		six: 0,
-		strikeRate: 0,
-	};
-	batsmanArray.push(batsmanOne);
+  let batsmanOne = {
+    name: cStricker.value,
+    run: 0,
+    bowl: 0,
+    four: 0,
+    six: 0,
+    strikeRate: 0,
+  };
+  batsmanArray.push(batsmanOne);
 
-	let nonStriker = document.getElementById("nonStriker");
-	let batsmanTw0 = {
-		name: nonStriker.value,
-		run: 0,
-		bowl: 0,
-		four: 0,
-		six: 0,
-		strikeRate: 0,
-	};
-	batsmanArray.push(batsmanTw0);
+  let nonStriker = document.getElementById("nonStriker");
+  let batsmanTw0 = {
+    name: nonStriker.value,
+    run: 0,
+    bowl: 0,
+    four: 0,
+    six: 0,
+    strikeRate: 0,
+  };
+  batsmanArray.push(batsmanTw0);
 
-	let oBowler = document.getElementById("oBowler");
-	let bowlerOne = {
-		name: oBowler.value,
-		over: 0,
-		maiden: 0,
-		run: 0,
-		wicket: 0,
-		economyRate: 0,
-	};
-	bowlerArray.push(bowlerOne);
+  let oBowler = document.getElementById("oBowler");
+  let bowlerOne = {
+    name: oBowler.value,
+    over: 0,
+    maiden: 0,
+    run: 0,
+    wicket: 0,
+    economyRate: 0,
+  };
+  bowlerArray.push(bowlerOne);
 
-	if (tossOwn == localStorage.getItem("HostName")) {
-		if (opted == "Bat") {
-			teams = JSON.parse(localStorage.getItem("teams"));
-			teams.forEach((team) => {
-				if (team.name == tossOwn) {
-					team.batsmanArray = batsmanArray;
-				}
-				else if (team.name == localStorage.getItem("VisitorName")){
-					team.bowlerArray =bowlerArray;
-				}
-			});
-			localStorage.setItem("teams", JSON.stringify(teams));
-		}
-		else{
-			teams = JSON.parse(localStorage.getItem("teams"));
-			teams.forEach((team) => {
-				if (team.name == tossOwn) {
-					team.bowlerArray = bowlerArray;
-				}
-				else if (team.name == localStorage.getItem("VisitorName")){
-					team.batsmanArray = batsmanArray;
-				}
+  let batBallTeamCheck = {};
 
-			});
-			localStorage.setItem("teams", JSON.stringify(teams));
-		}
-	}
-	else if(tossOwn == localStorage.getItem("VisitorName")){
-		if (opted == "Bat") {
-			teams = JSON.parse(localStorage.getItem("teams"));
-			teams.forEach((team) => {
-				if (team.name == tossOwn) {
-					team.batsmanArray = batsmanArray;
-				}
-				else if (team.name == localStorage.getItem("HostName")){
-					team.bowlerArray = bowlerArray;
-				}
-			});
-			localStorage.setItem("teams", JSON.stringify(teams));
-		}
-		else{
-			teams = JSON.parse(localStorage.getItem("teams"));
-			teams.forEach((team) => {
-				if (team.name == tossOwn) {
-					team.bowlerArray = bowlerArray;
-				}
-				else if(team.name == localStorage.getItem("HostName")){
-					team.batsmanArray = batsmanArray;
-				}
+  if (tossOwn == localStorage.getItem("HostName")) {
+    if (opted == "Bat") {
+      teams = JSON.parse(localStorage.getItem("teams"));
+      teams.forEach((team) => {
+        if (team.name == tossOwn) {
+          team.batsmanArray = batsmanArray;
+        } else if (team.name == localStorage.getItem("VisitorName")) {
+          team.bowlerArray = bowlerArray;
+        }
+      });
+      batBallTeamCheck = {
+        battingTeam: tossOwn,
+        bowlingTeam: localStorage.getItem("VisitorName"),
+      };
+      localStorage.setItem(
+        "batBallTeamCheck",
+        JSON.stringify(batBallTeamCheck)
+      );
+      localStorage.setItem("teams", JSON.stringify(teams));
+    } else {
+      teams = JSON.parse(localStorage.getItem("teams"));
+      teams.forEach((team) => {
+        if (team.name == tossOwn) {
+          team.bowlerArray = bowlerArray;
+        } else if (team.name == localStorage.getItem("VisitorName")) {
+          team.batsmanArray = batsmanArray;
+        }
+      });
+      batBallTeamCheck = {
+        battingTeam: localStorage.getItem("VisitorName"),
+        bowlingTeam: tossOwn,
+      };
+      localStorage.setItem(
+        "batBallTeamCheck",
+        JSON.stringify(batBallTeamCheck)
+      );
 
+      localStorage.setItem("teams", JSON.stringify(teams));
+    }
+  } else if (tossOwn == localStorage.getItem("VisitorName")) {
+    if (opted == "Bat") {
+      teams = JSON.parse(localStorage.getItem("teams"));
+      teams.forEach((team) => {
+        if (team.name == tossOwn) {
+          team.batsmanArray = batsmanArray;
+        } else if (team.name == localStorage.getItem("HostName")) {
+          team.bowlerArray = bowlerArray;
+        }
+      });
+      batBallTeamCheck = {
+        battingTeam: tossOwn,
+        bowlingTeam: localStorage.getItem("HostName"),
+      };
+      localStorage.setItem(
+        "batBallTeamCheck",
+        JSON.stringify(batBallTeamCheck)
+      );
 
+      localStorage.setItem("teams", JSON.stringify(teams));
+    } else {
+      teams = JSON.parse(localStorage.getItem("teams"));
+      teams.forEach((team) => {
+        if (team.name == tossOwn) {
+          team.bowlerArray = bowlerArray;
+        } else if (team.name == localStorage.getItem("HostName")) {
+          team.batsmanArray = batsmanArray;
+        }
+      });
+      batBallTeamCheck = {
+        battingTeam: localStorage.getItem("HostName"),
+        bowlingTeam: tossOwn,
+      };
+      localStorage.setItem(
+        "batBallTeamCheck",
+        JSON.stringify(batBallTeamCheck)
+      );
+      localStorage.setItem("teams", JSON.stringify(teams));
+    }
+  }
 
-			});
-			localStorage.setItem("teams", JSON.stringify(teams));
-		}
-	
-	}
+  //Store value
+  localStorage.setItem("batsmanOne", JSON.stringify(batsmanOne));
+  localStorage.setItem("batsmanTwo", JSON.stringify(batsmanTw0));
+  localStorage.setItem("bowlerOne", JSON.stringify(bowlerOne));
+  localStorage.setItem("currentBatsman", batsmanOne.name);
 
-	//Store value
-	localStorage.setItem("batsmanOne", JSON.stringify(batsmanOne));
-	localStorage.setItem("batsmanTwo", JSON.stringify(batsmanTw0));
-	localStorage.setItem("bowlerOne", JSON.stringify(bowlerOne));
-	localStorage.setItem("currentBatsman","batsmanOne");
-
-
-
-	if (cStricker.value != "" && nonStriker.value != "" && oBowler.value != "") {
-		location.href = "batvsbowl.html";
-	}
+  if (cStricker.value != "" && nonStriker.value != "" && oBowler.value != "") {
+    location.href = "batvsbowl.html";
+  }
 }
